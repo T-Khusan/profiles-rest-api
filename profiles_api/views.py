ce5810bc97d56cac7df61ea4a22e4c45db7dd4cb
Khusan Tojiboev <khusan.tojiboev@gmail.com>
2021-06-29 18:00:53 +0500

    Created models, views, serializers and ursl for ProfileFeedItem

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -5,8 +5,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, viewsets, filters
 
-from .serializers import HelloSerializer, UserProfileSerializer
-from .models import UserProfile
+from .serializers import HelloSerializer, UserProfileSerializer, ProfileFeedItemSerializer
+from .models import UserProfile, ProfileFeedItem
 from profiles_api import permissions
 
 class HelloApiView(APIView):
@@ -115,3 +115,15 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 class UserLoginApiView(ObtainAuthToken):
    """Handle creating user authentication tokens"""
    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    """Handles creating, reading and updating profile feed items"""
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = ProfileFeedItemSerializer
+    queryset = ProfileFeedItem.objects.all()
+
+    def perform_create(self, serializer):
+        """Sets the user profile to the logged in user. Default bor bu funtion,
+        user tomondan kelgan status_text ni saqlidi"""
+        serializer.save(user_profile=self.request.user)
